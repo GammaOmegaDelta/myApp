d404c8ad498d6090ba4edcd46362c19bb3cd613f
Adrian <a.fernandez1@live.com>
2019-06-05 16:04:55 -0700

    for each loop

--- Program.cs
+++ Program.cs
@@ -8,19 +8,16 @@ namespace CSharpFundamentals
 
         static void Main(string[] args)
         {
-           for (var i = 1; i <= 10; i++)
-            { 
-                if (i%2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-           for (var i = 10; i >= 1; i--)
-            { 
-                if (i%2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
+            var name = "Adrian Fernandez";
+
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
             }
         }
     }
