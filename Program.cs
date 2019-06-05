d6da5ce9b7c1d2a31b4283f3d0fe88d8cf7cc023
Adrian <a.fernandez1@live.com>
2019-06-05 16:20:57 -0700

    get.chomp in C#

--- Program.cs
+++ Program.cs
@@ -8,11 +8,32 @@ namespace CSharpFundamentals
 
         static void Main(string[] args)
         {
-            var numbers = new int[] { 1, 2, 3, 4 };
+            //for (var i =1; 1 <= 10, i++)
+            //{ 
+            //    if (i%2 ==0)
+            //        Console.WriteLine(i);
+            //}
+            //var i = 0;
+            //while (i <= 10)
+            //{ 
+            //    if (i %2 ==0)
+            //        Console.WriteLine(i);
 
-            foreach (var number in numbers)
-            {
-                Console.WriteLine(number);
+            //    i++;
+            //}
+            { 
+                while (true)
+                {
+                    Console.Write("Type your name: ");
+                    var input = Console.ReadLine();
+
+                    if (!String.IsNullOrWhiteSpace(input))
+                    {
+                        Console.WriteLine("@Echo: " + input);
+                        continue;
+                    }
+                    break;
+                }
             }
         }
     }
